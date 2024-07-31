3a0ff348c10163124f9e5543c7d903e6408348f9
goodthinglee <90130926+goodthinglee@users.noreply.github.com>
2024-07-31 21:01:51 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,13 +1,16 @@
 import java.io.*;
 import java.util.*;
-public class Main {
 
+/**
+ * 문자열을 입력받고 반복문으로 문자열을 쪼개서
+ * '('면 stack에 push
+ * ')'면 pop 또는 스택이 비어있으면 push하고 break
+ * 최종적으로 스택이 비어있으면 YES, 그렇지 않다면 NO
+ */
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-
-
 
         for(int i = 0; i<n; i++){
             Stack<Character> stack = new Stack<>();
