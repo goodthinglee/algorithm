2e35d96af237c3351f36f3f1dfa5422dbff7e634
goodthinglee <90130926+goodthinglee@users.noreply.github.com>
2024-08-16 20:04:01 +0900

    [Silver III] Title: N과 M (8), Time: 184 ms, Memory: 21836 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n,m;
+    static int[] arr;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        result = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        combination(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void combination(int start, int depth) {
+        if(depth == m){
+            for(int a : result){
+                sb.append(a + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = start; i<n; i++){
+                result[depth] = arr[i];
+                combination(i, depth + 1);
+        }
+    }
+}
