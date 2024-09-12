6642385355b9a76c6bea0bb1a95d2609d212dd28
goodthinglee <90130926+goodthinglee@users.noreply.github.com>
2024-09-12 20:11:38 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 224 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k =0; k<i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
